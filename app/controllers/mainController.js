6eb1ee857b65e385e28a6f97229d1aa1d4e2eed0
Stephsas <stephanie0marchand@gmail.com>
2024-06-05 11:33:12 +0200

    ajout BDD

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,9 +1,21 @@
 const mainController = {
-    homepage(req, res) {
-      res.render('index');
+    homePage(req, res) {
+      res.render('home');
     },
+
+    cataloguePage(req, res) {
+ res.render('catalogue');
+
+    },
+
+    articlePage(req, res) {
+      res.render('article');
+     
+         },
+
+
   };
   
-  // On exporte l'objet `mainController` qui contient une méthode `homepage`
+  // On exporte l'objet `mainController` qui contient les méthodes de rendu de mes views
   // On exporte le contenu de l'objet, pas le nom de la variable
   module.exports = mainController;
--- app/router.js
+++ app/router.js
@@ -7,6 +7,14 @@ const mainController = require('./controllers/mainController');
 const router = Router();
 
 // Ses routes appeleront les fonctions de nos controllers
-router.get('/', mainController.homepage);
+router.get('/', mainController.homePage);
+router.get('/catalogue', mainController.cataloguePage);
+router.get('/article/:id', mainController.articlePage);
+
+// une route pour afficher la page erreur
+
+router.use((req, res) => {
+    res.status(404).render('error');
+  });
 
 module.exports = router;
